8706be7167e4837cc51f4157012529cd95dda2d9
ioshY-k <joshyk1307@gmail.com>
2024-10-07 14:45:25 +0200

    Autotiling for floortiles

--- Scripts/spring_tool.gd
+++ Scripts/spring_tool.gd
@@ -3,14 +3,15 @@ extends RigidBody2D
 enum states {	IDLE, WALK, RUN, PUSH, JUMP, FALL, LAND,
 				WALLSLIDE_L, WALLSLIDE_R,
 				WALLJUMP_L, WALLJUMP_R}
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_player: AnimationPlayer = $Node2D/AnimationPlayer
+
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	animation_player.play("Bounce_anim")
 	var player: CharacterBody2D = body
 	player.controllable = false
-	player.position = position + Vector2(0,-100)
+	player.position = position + Vector2(0,-60)
 	player.velocity = Vector2.ZERO
 	await get_tree().create_timer(0.15).timeout
 	player.current_state = states.FALL
