da0ee3938c7bf39a23cffe36386389c22109406c
ioshY-k <joshyk1307@gmail.com>
2025-02-26 23:00:04 +0100

    v0.7.1 add 1 more lightning

--- Scripts/lightning.gd
+++ Scripts/lightning.gd
@@ -7,3 +7,7 @@ func _on_lightning_strike() -> void:
 	play("lightning_strike_anim")
 	animation_player.play("lightning_fade_anim")
 	print("strike!")
+
+
+func _on_lightning_timer_timeout() -> void:
+	pass # Replace with function body.
